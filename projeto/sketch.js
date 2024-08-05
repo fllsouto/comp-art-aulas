e0a7a4a7c3f8f1122d465123f2c50f91eb50076e
Fellipe Souto <fllsouto@gmail.com>
2024-08-05 20:35:39 -0300

    Updating files and uploading a new record for my class

--- projeto/sketch.js
+++ projeto/sketch.js
@@ -1,14 +1,15 @@
 function setup() {
-  s = 0.5
-  largura = 297
-  altura = 210
-  createCanvas(largura*(1 + s), altura*(1 + s));
+  s = 0.5
+  largura = 297
+  altura = 210
+  createCanvas(largura*(1 + s), altura*(1 + s));
 }
 
 function draw() {
-  background("gray");
-  strokeWeight(10); // Set the thickness of the point
-  x = 150 * (1 + s)
-  y = 105 * (1 + s)
-  point(x, y);  // Draw a point at coordinates (200, 200)
+  noLoop();
+  background("green")
+  fill("blue")
+  rect(30, 20, 55, 40);
+  fill("red")
+  rect(160, 140, 220, 160);
 }
